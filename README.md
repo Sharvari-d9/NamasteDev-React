6b425cb2058c0bf78288e51116509b6d5e6ba62e
Sharvari-d9 <sharvari.dongre9@gmail.com>
2023-09-04 01:23:55 +0530

    Added episode 1 notes

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # NamasteDev-React
+
+_What is CDN_
+
+- 
+
+_Cross Origin_
+
+
+_What is react_
+
+- Javascript library created by Meta developers
+
+- two scripts that are include , one is core react javascript code and other one is DOM development code
+
+- Philosophy with which react was created was to handle the most expensive function in any web application that is DOM manipulation. for ex: clicking a button can add or remove elements. All libraries try to optimize this as much as possible
+
---
+++ app.js
@@ -0,0 +1,27 @@
+// const heading1 = React.createElement("h1", {id : "heading"}, "Hello World! from React");
+// const root1 = ReactDOM.createRoot(document.getElementById("root"));
+// console.log(heading1); // createElement returns a javascript object
+
+// root1.render(heading1); // converts the object to h1 tag and add the returned object to the DOM
+
+/**
+ * <div id = "parent">
+ *  <div id = "child">
+ *      <h1>I am h1 tag </h1>
+ *   </div>
+ * </div>
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    [React.createElement("h1", {}, "This is h1 tag"),
+    React.createElement("h2", {}, "This is h2 tag")]
+  )
+);
+console.log(parent);
+const root1 = ReactDOM.createRoot(document.getElementById("root"));
+root1.render(parent);
