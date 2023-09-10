40fada5b3b0ce2941ace3cd88e656a88c9ce7772
Sharvari-d9 <sharvari.dongre9@gmail.com>
2023-09-11 02:07:27 +0530

    updated the files

--- app.js
+++ app.js
@@ -1,34 +1,9 @@
-// const heading1 = React.createElement("h1", {id : "heading"}, "Hello World! from React");
-// const root1 = ReactDOM.createRoot(document.getElementById("root"));
-// console.log(heading1); // createElement returns a javascript object
-
-// root1.render(heading1); // converts the object to h1 tag and add the returned object to the DOM
-
-/**
- * <div id = "parent">
- *  <div id = "child">
- *      <h1>I am h1 tag </h1>
- *   </div>
- * </div>
- */
 import React from "react";
-import ReactDOM from "react-dom";
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  [React.createElement(
-    "div",
-    { id: "child" },
-    [React.createElement("h1", {}, "This is Namaste React! "),
-    React.createElement("h2", {}, "by Akshay Saini")]
-  ),
-  React.createElement(
-    "div",
-    { id: "child" },
-    [React.createElement("h1", {}, "This is h1 tag "),
-    React.createElement("h2", {}, "This is h2 tag")]
-  )]
-);
-console.log(parent);
-const root1 = ReactDOM.createRoot(document.getElementById("root"));
-root1.render(parent);
+import ReactDOM from "react-dom/client";
+
+// React Element
+
+const heading = React.createElement("h1", { id: "heading" }, "Namaste React!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
