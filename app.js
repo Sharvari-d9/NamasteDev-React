1cbebe8c24767eac5950297281bf79e03a29a26f
Sharvari-d9 <sharvari.dongre9@gmail.com>
2023-09-18 16:27:57 +0530

    added Header Body along with CSS Styling

--- app.js
+++ app.js
@@ -4,44 +4,68 @@ import ReactDOM from "react-dom/client";
 /**
  * Head
  * -log
- * - Nav items 
- * 
+ * - Nav items
+ *
  * Body
  * -search bar
  * - restaurant container
  *  - restaurant card
- * 
- * Footer 
+ *  - tag
+ *  - Name of res, start rating
+ *  - cusine
+ *  - price
+ *
+ * Footer
  * - Links
  * - contact
  * - address
  * - copyright
  */
-const Header = () =>{
-    return(
-        <div className="header">
-            <div>
-                <img className="logo" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUjTTP11vgjnjicLVFtqRzFeenTFuQY_tnkcAkbQZVtm2eVQO1RHmh4uVZvCabzWt9oIQ&usqp=CAU"></img>
-            </div>
-            <div className="nav-items">
-            <ul>
-                <li>Home</li>
-                <li>About Us</li>
-                <li>Contact Us</li>
-                <li>Cart</li>
-            </ul>
-            </div>
-        </div>
-    )
-}
-
-const AppLayout = () =>{
-    return(
-        <div className="app">
-        <Header/>    
-        </div>
-    )
-
-}
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <img
+          className="logo"
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRUjTTP11vgjnjicLVFtqRzFeenTFuQY_tnkcAkbQZVtm2eVQO1RHmh4uVZvCabzWt9oIQ&usqp=CAU"
+        ></img>
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+const RestaurantCard = () => {
+  return (
+    <div className="restaurant-card" style={{backgroundColor: "beige"}}>
+      <img className="restaurant-logo" alt="Aroma Picture" src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/jdfi5bh0hmdet7g7qk5z"></img>
+      <h3>Aroma's Hyderabad House</h3>
+      <h4>Biryani, North Indian</h4>
+    </div>
+  );
+};
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search-bar">Search</div>
+      <div className="restaurant-container">Restaurant Container</div>
+      <RestaurantCard />
+    </div>
+  );
+};
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppLayout/>);
+root.render(<AppLayout />);
